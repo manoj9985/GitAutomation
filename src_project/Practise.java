153fefd269f7bec61a493172d5a6391e11776f59
Admin <manojmanu.kumar27@gmail.com>
2025-06-12 21:57:31 +0530

    Modified test commited

--- src_project/Practise.java
+++ src_project/Practise.java
@@ -2,17 +2,8 @@ public class Practise {
 
 	public static void main(String[] args) {
 		
-		int num = 64;
-		int rem = 0;
-		
-		while (num >0)
-		{
-			rem = rem+num%10;
-			num = num/10;
-		}
-		
-				
-		System.out.println(rem);
+
+		System.out.println("This is to check the GIT code");
 		
 		
 	}

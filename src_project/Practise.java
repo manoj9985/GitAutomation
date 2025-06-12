a944e1e49026449112e8b704af57bd321eca0cf1
Admin <manojmanu.kumar27@gmail.com>
2025-06-12 23:20:18 +0530

    Master code push

--- src_project/Practise.java
+++ src_project/Practise.java
@@ -3,26 +3,7 @@ public class Practise {
 	public static void main(String[] args) {
 		
 
-		System.out.println("This is to check the GIT code");
-
-		System.out.println("This is to check the GIT code");
-
-		System.out.println("This is to check the GIT code");
-
-		System.out.println("This is to check the GIT code");
-
-		System.out.println("This is to check the GIT code");
-
-		System.out.println("This is to check the GIT code");
-
-		System.out.println("This is to check the GIT code");
-
-		System.out.println("This is to check the GIT code");
-		System.out.println("This is to check the GIT code");
-		System.out.println("This is to check the GIT code");
-		System.out.println("This is to check the GIT code");
-		System.out.println("This is to check the GIT code");
-		System.out.println("This is to check the GIT code");
+		System.out.println("GIT origin master");
 		
 
 		

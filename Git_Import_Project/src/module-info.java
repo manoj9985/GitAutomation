00806c3b344621f1fb0b67f7e52dfcf59bf9e78a
Admin <manojmanu.kumar27@gmail.com>
2025-06-13 16:08:36 +0530

    New push 13-06

---
+++ Git_Import_Project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Git_Import_Project {
+}
--- src_project/Practise.java
+++ src_project/Practise.java
@@ -2,13 +2,9 @@ public class Practise {
 
 	public static void main(String[] args) {
 		
+		System.out.println("This is to check the created branch changes");
+		System.out.println("This is to check the created branch changes");
 
-		System.out.println("GIT origin master");
-		System.out.println("GIT origin master2");
-		System.out.println("GIT origin master3");
-		
-
-		
 		
 	}
 
